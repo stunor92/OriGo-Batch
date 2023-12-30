5393762cae1a7f8827ccae1c710d7ce4406faa71
Sturle Tolo Nordeide <positurer-rektangler0y@icloud.com>
2023-12-31 00:29:04 +0100

    Reset cronjob

--- src/main/java/no/stunor/origo/batch/sceduler/BatchScheduler.java
+++ src/main/java/no/stunor/origo/batch/sceduler/BatchScheduler.java
@@ -28,7 +28,7 @@ public record BatchScheduler(
     OrganisationService organisationService) {    
 
 
-    @Scheduled(cron="0 9 0 * * *")
+    @Scheduled(cron="0 0 3 * * *")
     public void sceduleJob() throws InterruptedException, ExecutionException, EventorApiException{
         log.info("Start batch job...");
         Timestamp startTtme = Timestamp.now();
