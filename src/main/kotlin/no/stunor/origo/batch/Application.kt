a92b487578b2d84a29b2c55354c4626407cea4ff
Sturle Tolo Nordeide <110419122+stunor92@users.noreply.github.com>
2025-06-28 00:14:09 +0200

    feat: Make sure the app shouts down after finishes

--- src/main/kotlin/no/stunor/origo/batch/Application.kt
+++ src/main/kotlin/no/stunor/origo/batch/Application.kt
@@ -4,12 +4,19 @@ import no.stunor.origo.batch.services.BatchService
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.ApplicationContext
+import kotlin.system.exitProcess
 
 @SpringBootApplication
-open class Application private constructor(private val batchService: BatchService) : CommandLineRunner {
+open class Application private constructor(
+    private val batchService: BatchService,
+    private val applicationContext: ApplicationContext
+) : CommandLineRunner {
 
     override fun run(vararg args: String?) {
         batchService.updateOrganisations()
+        val exitCode = SpringApplication.exit(applicationContext)
+        exitProcess(exitCode)
     }
 }
 
