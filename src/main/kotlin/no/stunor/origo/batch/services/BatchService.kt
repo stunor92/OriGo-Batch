d03f7f0ca4f1d0654f00c1f3434d716f67e8cbef
Sturle Tolo Nordeide <110419122+stunor92@users.noreply.github.com>
2025-06-28 13:30:01 +0200

    fix: Do not specify postges dialect and remove acurator

--- src/main/kotlin/no/stunor/origo/batch/services/BatchService.kt
+++ src/main/kotlin/no/stunor/origo/batch/services/BatchService.kt
@@ -5,8 +5,6 @@ import no.stunor.origo.batch.data.EventorRepository
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RestController
 
 @Service
 internal class BatchService {
